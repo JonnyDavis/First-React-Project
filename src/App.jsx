4b8f846704c0f6e554f962364ff5d176439135ea
JonnyDavis <jonnydavis39@yahoo.co.uk>
2017-11-27 12:35:46 +0000

    Added component: Stuff (groups all sub-components into one component)

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Paragraph from "./Paragraph";
-import Square from "./Square";
-import People from "./People";
+import Stuff from "./Stuff";
+
 
 const App = () => (
     <div>
-      <Header />
-      <Paragraph />
-      <Square />
-      <People />
+      <Stuff />
     </div>
 
 );  
---
+++ src/Stuff.jsx
@@ -0,0 +1,21 @@
+// import the React library
+import React from "react";
+import Header from "./Header";
+import Paragraph from "./Paragraph";
+import Square from "./Square";
+import People from "./People";
+
+
+// create our component
+// just a function that returns JSX
+const App = () => (
+    <div>
+        <Header />
+        <Paragraph />
+        <Square />
+        <People />
+    </div>
+);
+
+// export our component
+export default App;
